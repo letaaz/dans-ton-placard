df5f666342c6f734dc63e8b62cd566c9a2fae407
elati <fouad.el-ati@etudiant.univ-lille1.fr>
2019-01-14 12:08:23 +0100

    fix: Adding ray in strings.xml and code getRayon method in Rayon.java modified

--- app/src/main/java/com/sem/lamoot/elati/danstonplacard/danstonplacard/database/model/Rayon.java
+++ app/src/main/java/com/sem/lamoot/elati/danstonplacard/danstonplacard/database/model/Rayon.java
@@ -14,36 +14,37 @@ public enum Rayon {
     }
 
     public static Rayon getRayon(String rayon) {
-        if (rayon.equals("SURGELE") || rayon.equals("Surgelés"))
-            return SURGELE;
-        if (rayon.equals("BIO") || rayon.equals("Bio"))
-            return BIO;
-        if (rayon.equals("FRUITS_LEGUMES") || rayon.equals("Fruits et Légumes"))
-            return FRUITS_LEGUMES;
-        if (rayon.equals("BOISSONS") || rayon.equals("Boissons"))
-            return BOISSONS;
-        if (rayon.equals("BOULANGERIE_PATISSERIE") || rayon.equals("Boulangerie et Patisserie"))
-            return BOULANGERIE_PATISSERIE;
-        if (rayon.equals("CREMERIE") || rayon.equals("Fruits ou Crèmerie"))
-            return CREMERIE;
-        if (rayon.equals("YAOURTS_DESSERTS") || rayon.equals("Yaourts et Desserts"))
-            return YAOURTS_DESSERTS;
-        if (rayon.equals("FROMAGES") || rayon.equals("Fromages"))
-            return FROMAGES;
-        if (rayon.equals("VIANDES") || rayon.equals("Viandes"))
-            return VIANDES;
-        if (rayon.equals("POISSONS_CRUSTACES") || rayon.equals("Poissons et Crustaces"))
-            return POISSONS_CRUSTACES;
-        if (rayon.equals("EPICERIE_SUCREE") || rayon.equals("Épicerie sucrée"))
-            return EPICERIE_SUCREE;
-        if (rayon.equals("EPICERIE_SALEE") || rayon.equals("Épicerie salée"))
-            return EPICERIE_SALEE;
-        if (rayon.equals("BEAUTE") || rayon.equals("Beauté"))
-            return BEAUTE;
-        if (rayon.equals("DIVERS") || rayon.equals("Divers"))
-            return DIVERS;
-        else
-            return null;
+        switch(rayon){
+            case "SURGELE": case "Surgelés":
+                return SURGELE;
+            case "BIO": case "Bio":
+                return BIO;
+            case "FRUITS ET LEGUMES": case "Fruits et Légumes":
+                return FRUITS_LEGUMES;
+            case "BOISSONS": case "Boissons":
+                return BOISSONS;
+            case "BOULANGERIE ET PATISSERIE": case "Boulangerie et Patisserie":
+                return BOULANGERIE_PATISSERIE;
+            case "CREMERIE" : case "Crèmerie":
+                return CREMERIE;
+            case "YAOURTS ET DESSERTS": case "Yaourts et Desserts":
+                return YAOURTS_DESSERTS;
+            case "FROMAGES": case "Fromages":
+                return FROMAGES;
+            case "VIANDES": case "Viandes":
+                return VIANDES;
+            case "POISSONS ET CRUSTACES": case "Poissons et Crustaces":
+                return POISSONS_CRUSTACES;
+            case "EPICERIE SUCREE": case "Épicerie sucrée":
+                return EPICERIE_SUCREE;
+            case "EPICERIE SALEE": case "Épicerie salée":
+                return EPICERIE_SALEE;
+            case "BEAUTE": case "Beauté":
+                return BEAUTE;
+            default:
+                return DIVERS;
+        }
+
     }
 
 
