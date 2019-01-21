087938a404d1d79d9fa39f465dbd10fd93200270
Unknown <semgjj@yahoo.fr>
2019-01-21 23:45:42 +0100

    feat : navigation drawer + default content
    organize navigation drawer items
    default page for others fragments

---
+++ app/src/main/java/com/sem/lamoot/elati/danstonplacard/danstonplacard/AboutActivity.java
@@ -0,0 +1,28 @@
+package com.sem.lamoot.elati.danstonplacard.danstonplacard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ImageView close_btn = findViewById(R.id.about_close_btn);
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        finish();
+    }
+}
